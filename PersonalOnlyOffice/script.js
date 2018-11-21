86a9500339e5055ee31b765667f373001d195809
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2018-11-21 16:46:12 +0300

    Скрипт работает.

--- PersonalOnlyOffice/script.js
+++ PersonalOnlyOffice/script.js
@@ -1,15 +1,19 @@
 var stickydivPos = document.getElementById("stickydiv");
-setInterval(function () {
+var style = getComputedStyle(stickydivPos);
+//var stickydivTop = parseInt(style.top);
+window.onscroll = function() {
     var html = document.documentElement;
     var body = document.body;
     var scrollTop = html.scrollTop || body && body.scrollTop || 0;
     scrollTop -= html.clientTop; // в IE7- <html> смещён относительно (0,0)
     //var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop >= 410) {
+    stickydivPos.style.top = "";
+    var stickydivTop = parseInt(style.top);
+    if (scrollTop >= stickydivTop) {
         stickydivPos.style.position = "fixed";
         stickydivPos.style.top = "0";
     } else {
         stickydivPos.style.position = "";
         stickydivPos.style.top = "";
     };
-}, 1);
+};
