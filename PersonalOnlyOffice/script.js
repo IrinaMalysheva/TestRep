3a34d693224d0469b38cde7f5e14b61778ca1e1f
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2018-11-21 18:33:29 +0300

    Написано "прилипание", подключена jquery-3.3.1.min.js.

--- PersonalOnlyOffice/script.js
+++ PersonalOnlyOffice/script.js
@@ -1,19 +1,35 @@
-var stickydivPos = document.getElementById("stickydiv");
-var style = getComputedStyle(stickydivPos);
-//var stickydivTop = parseInt(style.top);
-window.onscroll = function() {
-    var html = document.documentElement;
-    var body = document.body;
-    var scrollTop = html.scrollTop || body && body.scrollTop || 0;
-    scrollTop -= html.clientTop; // в IE7- <html> смещён относительно (0,0)
-    //var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    stickydivPos.style.top = "";
-    var stickydivTop = parseInt(style.top);
-    if (scrollTop >= stickydivTop) {
-        stickydivPos.style.position = "fixed";
-        stickydivPos.style.top = "0";
-    } else {
-        stickydivPos.style.position = "";
-        stickydivPos.style.top = "";
+STICK = (function() {
+    var stickydivPos, style, stickydivTop;
+    
+    var init = function() {
+        stickydivPos = document.getElementById("stickydiv");
+        style = getComputedStyle(stickydivPos);
+        stickydivTop = parseInt(style.top);
+        window.onscroll = onscroll;
+    }
+
+    function onscroll() {
+        var html = document.documentElement;
+        var body = document.body;
+        var scrollTop = html.scrollTop || body && body.scrollTop || 0;
+        scrollTop -= html.clientTop; // в IE7- <html> смещён относительно (0,0)
+        //var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        var position = "", top = "";
+        if (scrollTop >= stickydivTop) {
+            position = "fixed";
+            top = "0";
+        }
+
+        stickydivPos.style.position = position;
+        stickydivPos.style.top = top;
+    }
+
+    
+    return {
+        init: init
     };
-};
+
+})();
+
+window.addEventListener("load", STICK.init);
