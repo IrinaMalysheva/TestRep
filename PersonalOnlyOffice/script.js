f595de0aa8cff215a17a6e4c78a9500c568247b9
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2018-11-22 18:45:58 +0300

    Переписан .js на JQuery, добавлена первая карусель (серый блок).

--- PersonalOnlyOffice/script.js
+++ PersonalOnlyOffice/script.js
@@ -1,35 +1,56 @@
 STICK = (function() {
-    var stickydivPos, style, stickydivTop;
+    var stickydivTop;
     
     var init = function() {
-        stickydivPos = document.getElementById("stickydiv");
-        style = getComputedStyle(stickydivPos);
-        stickydivTop = parseInt(style.top);
+        stickydivTop =parseInt($("#stickydiv").css("top"));
         window.onscroll = onscroll;
     }
 
     function onscroll() {
-        var html = document.documentElement;
-        var body = document.body;
-        var scrollTop = html.scrollTop || body && body.scrollTop || 0;
-        scrollTop -= html.clientTop; // в IE7- <html> смещён относительно (0,0)
-        //var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
         
         var position = "", top = "";
-        if (scrollTop >= stickydivTop) {
+        if ($(window).scrollTop() >= stickydivTop) {
             position = "fixed";
             top = "0";
         }
 
-        stickydivPos.style.position = position;
-        stickydivPos.style.top = top;
+        $("#stickydiv").css("position",position);
+        $("#stickydiv").css("top",top);
     }
 
-    
     return {
         init: init
     };
 
 })();
 
-window.addEventListener("load", STICK.init);
+$(document).ready(function(){
+    STICK.init();
+});
+
+// $(document).ready(function(){
+
+//     var stickydivPos, style, stickydivTop;
+    
+//     stickydivPos = document.getElementById("stickydiv");
+//     style = getComputedStyle(stickydivPos);
+//     stickydivTop = parseInt(style.top);
+//     window.onscroll = onscroll;
+
+//     function onscroll() {
+//         var html = document.documentElement;
+//         var body = document.body;
+//         var scrollTop = html.scrollTop || body && body.scrollTop || 0;
+//         scrollTop -= html.clientTop; // в IE7- <html> смещён относительно (0,0)
+        
+//         var position = "", top = "";
+//         if (scrollTop >= stickydivTop) {
+//             position = "fixed";
+//             top = "0";
+//         }
+
+//         stickydivPos.style.position = position;
+//         stickydivPos.style.top = top;
+//     }
+    
+// });
