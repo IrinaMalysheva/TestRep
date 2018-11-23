7e9c65c3dd2e060e2429ea45aad3484af143a501
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2018-11-23 18:45:34 +0300

    Попытка написания скрипта для "адаптивной" 3-ей карусели - пока работает некорректно.

--- PersonalOnlyOffice/carousel.js
+++ PersonalOnlyOffice/carousel.js
@@ -6,11 +6,73 @@ $(document).ready(function(){
         dots: true,
         dotsClass: "dots-carousel",
     });
-    $('.carousel-user-exp').slick({
-        infinite: true,
-        slidesToShow: 2,
-        appendArrows: $('.arrows-user-exp'),
-        prevArrow: '<button id="prev-cust" type="button" class="prev-click"></button>',
-        nextArrow: '<button id="next-cust" type="button" class="next-click"></button>',
-    });
+
+    CUE = (function() {
+        
+        var choseCarUserExp = function() {
+            window.onresize = onresize;
+        }
+    
+        function onresize() {
+            if (window.innerWidth > 1040) {
+                $(function () {
+                    $(".carousel-user-exp").slick({
+                        infinite: true,
+                        slidesToShow: 2,
+                        appendArrows: $('.arrows-user-exp'),
+                        prevArrow: '<button id="prev-cust" type="button" class="prev-click"></button>',
+                        nextArrow: '<button id="next-cust" type="button" class="next-click"></button>',            
+                    });
+                });
+            } else {
+                $(function () {
+                    $(".carousel-user-exp").slick({
+                        appendArrows: $('.arrows-user-exp'),
+                        prevArrow: '<button id="prev-cust" type="button" class="prev-click"></button>',
+                        nextArrow: '<button id="next-cust" type="button" class="next-click"></button>',
+                    });
+                });
+            }
+        }
+    
+        return {
+            choseCarUserExp: choseCarUserExp
+        };
+    
+    })();
+
+    CUE.choseCarUserExp();
+
+    
+    // function checkbodyWidth() {
+    //     if (window.innerWidth > 1040) {
+    //         $(function () {
+    //             $(".carousel-user-exp").slick({
+    //                 infinite: true,
+    //                 slidesToShow: 2,
+    //                 appendArrows: $('.arrows-user-exp'),
+    //                 prevArrow: '<button id="prev-cust" type="button" class="prev-click"></button>',
+    //                 nextArrow: '<button id="next-cust" type="button" class="next-click"></button>',            
+    //             });
+    //         });
+    //     } else {
+    //         $(function () {
+    //             $(".carousel-user-exp").slick({
+    //                 appendArrows: $('.arrows-user-exp'),
+    //                 prevArrow: '<button id="prev-cust" type="button" class="prev-click"></button>',
+    //                 nextArrow: '<button id="next-cust" type="button" class="next-click"></button>',
+    //             });
+    //         });
+    //     }
+    // }
+    // //checkbodyWidth();
+    // $(window).resize(checkbodyWidth());
+
+    // $('.carousel-user-exp').slick({
+    //     infinite: true,
+    //     slidesToShow: 2,
+    //     appendArrows: $('.arrows-user-exp'),
+    //     prevArrow: '<button id="prev-cust" type="button" class="prev-click"></button>',
+    //     nextArrow: '<button id="next-cust" type="button" class="next-click"></button>',
+    // });
 });
